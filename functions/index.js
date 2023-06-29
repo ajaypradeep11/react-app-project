ded5fe1980f37bbe3a2097dc1179c7438a45f7c4
mpradeep11 <mpradeep11@gmail.com>
2023-06-28 22:36:15 -0400

    additional changes

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,13 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 const functions = require("firebase-functions");
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
+
 admin.initializeApp()
 
 exports.createStripeCheckout = functions.https.onCall(async (data, context) => {
-    // Stripe init
+    const receivedData = data; // Access the received data
     const stripe = require("stripe")('sk_test_51NLfCWHaVQ7a8uC8yosWfJgS0fqgoDbMfWH55Ftp3WxpPgXTsaYPUWPeDDv04RioNzawctKshtOg6a5l45Lba42Q00t8uhmelG');
+    console.log("wasssup ", receivedData);
+    
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
@@ -21,9 +16,9 @@ exports.createStripeCheckout = functions.https.onCall(async (data, context) => {
       
       line_items: [
         {
-          quantity: 1,
+          quantity: receivedData,
           price_data: {
-            currency: "usd",
+            currency: "cad",
             unit_amount: (100) * 1, // 10000 = 100 USD
             product_data: {
               name: "Bulk Service Files",
@@ -40,32 +35,18 @@ exports.createStripeCheckout = functions.https.onCall(async (data, context) => {
   
 
 exports.events = functions.https.onRequest(async (req, res) => {
-
-    const stripe = require("stripe")('sk_test_51NLfCWHaVQ7a8uC8yosWfJgS0fqgoDbMfWH55Ftp3WxpPgXTsaYPUWPeDDv04RioNzawctKshtOg6a5l45Lba42Q00t8uhmelG');
-    let event;
-  
     try {
-      const whSec = 'whsec_Qn58H3qK61G33UfcHW1ZsGvXrCaIB1yu';
-  
-      event = stripe.webhooks.constructEvent(
-        req.rawBody,
-        req.headers["stripe-signature"],
-          whSec,
-      );
-      
-    } catch (err) {
-      console.error("Signature Failed.");
+      const doc = admin.firestore().collection("user").doc("user")
+      const data = await doc.get();
+      const value = data.data();
+      const inc = value.available_service_count + 1;
+      await admin.firestore().collection("user").doc("user").set({
+        available_service_count: inc,
+      });
+      return res.status(200).json({ data });
+    }
+    catch (err) {
+      console.error("Error");
       return res.sendStatus(400);
     }
-
-    //const dataObject = event.data.object;
-
-    await admin.firestore().collection("user").doc().set({
-      available_service_count: 1,
-    });
-  
-    return res.sendStatus(200);
-
-
-
 }); 
--- src/views/purchase/purchase.js
+++ src/views/purchase/purchase.js
@@ -14,10 +14,7 @@ import {
     CModalBody,
     CFormLabel,
     CModalFooter,
-    CInputGroupText,
-    CInputGroup,
     CHeaderBrand,
-    CHeaderToggler,
     CFormSwitch,
     CFormSelect,
     CContainer,
@@ -27,49 +24,83 @@ import { useState, useEffect } from 'react';
 import { db } from '../firebase/firebase-config';
 import { collection, getDocs, doc } from 'firebase/firestore';
 import { getFunctions, httpsCallable } from 'firebase/functions';
-import { loadStripe } from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js';
 
 const Purchase = () => {
     const [visible, setVisible] = useState(false);
-    const [users, setUsers] = useState([]);
-    const usersCollectionRef = collection(db, "user");
+    const [users, setUsers] = useState([]); 
+    const [inputValue, setInputValue] = useState('');
+    const [filename, setFilename] = useState('');
+    const [dateid, setDateid] = useState('');
+    const [fileno, setFileno] = useState('');
+    const [address, setAddress] = useState('');
+    
+
     const createStripeCheckout = httpsCallable(getFunctions(), 'createStripeCheckout');
-    const stripe = loadStripe('pk_test_51NLfCWHaVQ7a8uC8g1BD8h7Jwx2JFEMuIeUFWHNFaJfeaIy01l0r9sDPCBI7eI9w4VxINWwunnmjxdsOoeauVIYE00zhmPolPI')
+    const usersCollectionRef = collection(db, "user");
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    };
+
+    const handleDateChange = (e) => {
+        setDateid(e.target.value);
+    };
+    const handleFileChange = (e) => {
+        setFilename(e.target.value);
+    };
+    const handleFileNoChange = (e) => {
+        setFileno(e.target.value);
+    };
+    const handleComboChange = (e) => {
+        setAddress(e.target.value);
+    };
+
+    const handleOnClickCreate = () => {
+        console.log("lol");
+       setVisible(!visible)
+    }
 
     useEffect(() => {
         const getUsers = async () => {
             const data = await getDocs(usersCollectionRef);
             setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-            console.log(data);
         };
-
         getUsers();
     }, []);
 
 
     useEffect(() => {
         const initializeStripe = async () => {
-          const stripe = await loadStripe('pk_test_51NLfCWHaVQ7a8uC8g1BD8h7Jwx2JFEMuIeUFWHNFaJfeaIy01l0r9sDPCBI7eI9w4VxINWwunnmjxdsOoeauVIYE00zhmPolPI');
-    
-          const handleClick = async () => {
-            // Create a checkout session
+            const stripe = await loadStripe('pk_test_51NLfCWHaVQ7a8uC8g1BD8h7Jwx2JFEMuIeUFWHNFaJfeaIy01l0r9sDPCBI7eI9w4VxINWwunnmjxdsOoeauVIYE00zhmPolPI');
+            var dataValue = 0;
+            if(inputValue == "") {
+              dataValue = 1;
+            }else {
+              dataValue = inputValue;
+            }
             
+            const handleClick = async () => {               
+                createStripeCheckout(dataValue)
+                    .then(response => {
+                        stripe.redirectToCheckout({ sessionId: response.data.id })
+                    })
 
-            createStripeCheckout()
-            .then(response => {
-                const sessionId = response.data.id
-                stripe.redirectToCheckout({ sessionId: sessionId })
-            }) 
 
-          };
-    
-          // Attach click event listener to your checkout button
-          const checkoutButton = document.getElementById('buy-button');
-          checkoutButton.addEventListener('click', handleClick);
+            };
+            const buyButton = document.getElementById('buy-button');
+            buyButton.addEventListener('click', handleClick);
         };
-    
+
         initializeStripe();
-      }, []);
+    }, [inputValue]);
+
+
+
 
     return (
         <CContainer>
@@ -83,90 +114,89 @@ const Purchase = () => {
                                     <div key={user.id}>You can create {user.available_service_count} File</div>
                                 );
                             })}
-
                         </CCardTitle>
                         <CCardText>Create a new file to get started or buy more files</CCardText>
                         <CRow>
                             <CCol sm={10}>
-                                <CFormInput type="text" placeholder="" aria-label="default input example" />
+                                <CFormInput onChange={handleInputChange} type="text" placeholder="" aria-label="default input example" />
                             </CCol>
                             <CCol sm={2}>
 
                                 <CButton id="buy-button">Buy</CButton>
-                        </CCol>
-                    </CRow>
-                </CCardBody>
-            </CCard>
-            <CButton onClick={() => setVisible(!visible)}>Create a new File</CButton>
-            <CModal size="xl" alignment="center" visible={visible} onClose={() => setVisible(false)}>
-                <CModalHeader onClose={() => setVisible(false)}>
-                    <CModalTitle >Create a new file</CModalTitle>
-                </CModalHeader>
-                <CModalBody>
-                    <CRow className="mb-3">
-                        <CFormLabel htmlFor="staticEmail" className="col-sm-2 col-form-label">Type of File</CFormLabel>
-                        <CCol sm={10}>
-                            <CButton color="dark" variant="outline">Sale</CButton>
-                            <CButton color="dark" variant="outline">Purchase</CButton>
-                            <CButton color="dark" variant="outline">Mortgage</CButton>
-
-                        </CCol>
-                    </CRow>
-                    <CRow className="mb-3">
-                        <CFormLabel htmlFor="filename" className="col-sm-2 col-form-label">Name of file</CFormLabel>
-                        <CCol sm={10}>
-                            <CFormInput type="text" id="filename" />
-                        </CCol>
-                    </CRow>
-                    <CRow className="mb-3">
-                        <CFormLabel htmlFor="dateid" className="col-sm-2 col-form-label">Closing Date</CFormLabel>
-                        <CCol sm={10}>
-                            <CFormInput type="date" id="dateid" />
-                        </CCol>
-                    </CRow>
-                    <CRow className="mb-3">
-                        <CFormLabel htmlFor="fileno" className="col-sm-2 col-form-label">File no.</CFormLabel>
-                        <CCol sm={10}>
-                            <CFormInput type="password" id="fileno" />
-                        </CCol>
-                    </CRow>
-
+                            </CCol>
+                        </CRow>
+                    </CCardBody>
+                </CCard>
+                <CButton onClick={handleOnClickCreate}>Create a new File</CButton>
+                <CModal size="xl" alignment="center" visible={visible} onClose={() => setVisible(false)}>
+                    <CModalHeader onClose={() => setVisible(false)}>
+                        <CModalTitle >Create a new file</CModalTitle>
+                    </CModalHeader>
+                    <CModalBody>
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="staticEmail" className="col-sm-2 col-form-label">Type of File</CFormLabel>
+                            <CCol sm={10}>
+                                <CButton color="dark" variant="outline">Sale</CButton>
+                                <CButton color="dark" variant="outline">Purchase</CButton>
+                                <CButton color="dark" variant="outline">Mortgage</CButton>
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="filename" className="col-sm-2 col-form-label">Name of file</CFormLabel>
+                            <CCol sm={10}>
+                                <CFormInput type="text" id="filename" onChange={handleFileChange} value={filename}/>
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="dateid" className="col-sm-2 col-form-label">Closing Date</CFormLabel>
+                            <CCol sm={10}>
+                                <CFormInput type="date" id="dateid" onChange={handleDateChange} value={dateid}/>
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="fileno" className="col-sm-2 col-form-label">File no.</CFormLabel>
+                            <CCol sm={10}>
+                                <CFormInput type="password" id="fileno" onChange={handleFileNoChange} value={fileno}/>
+                            </CCol>
+                        </CRow>
 
 
-                    <CRow className="mb-3">
-                        <CHeader>
-                            <CHeaderBrand href="#">Subject Property</CHeaderBrand>
-                        </CHeader>
-                    </CRow>
+                        <CRow className="mb-3">
+                            <CHeader>
+                                <CHeaderBrand href="#">Subject Property</CHeaderBrand>
+                            </CHeader>
+                        </CRow>
 
-                    <CRow className="mb-3">
-                        <CFormLabel htmlFor="fileno" className="col-sm-2 col-form-label">Address</CFormLabel>
-                        <CCol sm={9}>
-                            <CFormSelect
-                                aria-label="Default select example"
-                                options={[
-                                    'Open this select menu',
-                                    { label: 'One', value: '1' },
-                                    { label: 'Two', value: '2' },
-                                    { label: 'Three', value: '3', disabled: true }
-                                ]}
-                            />
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="address" className="col-sm-2 col-form-label">Address</CFormLabel>
+                            <CCol sm={9}>
+                                <CFormSelect
+                                value={address}
+                                onChange={handleComboChange}
+                                    aria-label="Default select example"
+                                    options={[
+                                        '',
+                                        { label: '1', value: '1' },
+                                        { label: '2', value: '2' },
+                                        { label: '3', value: '3', disabled: true }
+                                    ]}
+                                />
 
-                        </CCol>
-                        <CCol sm={1}>
-                            <CFormSwitch size="xl" label="" id="formSwitchCheckDefaultXL" />
-                        </CCol>
-                    </CRow>
+                            </CCol>
+                            <CCol sm={1}>
+                                <CFormSwitch size="xl" label="" id="formSwitchCheckDefaultXL" />
+                            </CCol>
+                        </CRow>
 
-                </CModalBody>
-                <CModalFooter>
-                    <CButton color="secondary" onClick={() => setVisible(false)}>
-                        Cancel
-                    </CButton>
-                    <CButton color="primary">Create</CButton>
-                </CModalFooter>
-            </CModal>
-        </CRow>
+                    </CModalBody>
+                    <CModalFooter>
+                        <CButton color="secondary" onClick={() => setVisible(false)}>
+                            Cancel
+                        </CButton>
+                        <CButton color="primary">Create</CButton>
+                    </CModalFooter>
+                </CModal>
+            </CRow>
         </CContainer >
     )
 }
